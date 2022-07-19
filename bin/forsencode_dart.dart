9728b87052fba10fc465f644273e5691651d1e81
Mykola <ks2225@protonmail.com>
2022-07-20 02:25:29 +0300

    feat: add file import for cli

--- bin/forsencode_dart.dart
+++ bin/forsencode_dart.dart
@@ -1,6 +1,12 @@
 import 'package:forsencode_dart/forsencode_dart.dart' as forsencode_dart;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  var stdin = arguments.join(" ");
-  print(forsencode_dart.toForsenCode(stdin));
+void main(List<String> arguments) async {
+  try {
+    var file = await File(arguments.join(" ")).readAsString();
+    print(forsencode_dart.toForsenCode(file));
+  } on FileSystemException {
+    print("Error: File not entered");
+    exit(1);
+  }
 }
